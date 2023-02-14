cc6aadc45ffcf47a88eea5bdd2bdb492cf9115cd
Anastasiia Lytvyn <lytvynana@gmail.com>
2023-02-15 00:39:47 +0200

    add function for checking go vet command

--- check_actions.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var i int
-	fmt.Println(i == 0 && i == 0)
-}
--- main.go
+++ main.go
@@ -5,3 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+func checkActions() {
+	var i int
+	fmt.Println(i == 0 && i == 0)
+}
